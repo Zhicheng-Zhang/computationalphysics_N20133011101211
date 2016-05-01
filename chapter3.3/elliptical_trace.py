c78fce06796492b10a10d292cbf9861a8e1e2f45
Zhicheng <2802720706@qq.com>
2016-05-01 14:29:38 +0800

    more pictures

--- chapter3.3/elliptical_trace.py
+++ chapter3.3/elliptical_trace.py
@@ -1,5 +1,6 @@
 from math import*
 from visual import*
+from matplotlib.pyplot import*
 
 class  Billiard():
     ''' An elliptical billiard table'''
@@ -7,9 +8,10 @@ def __init__(self,dt):
     self.dt=dt
 
 def collision(self,theta0,run_t):
+    x=[];v_x=[];y=[]
     t=arange(0,2*pi,0.1)
     curve(x=20*cos(t),y=10*sin(t),z=0,color=color.red)
-    ball=sphere(pos=(0,0,0),radius=1,color=color.blue)
+    ball=sphere(pos=(10*sqrt(3),0,0),radius=1,color=color.blue)
     ball.velocity=vector(1*cos(theta0),1*sin(theta0),0)
     ball.trail=curve(color=ball.color)
     for i in range(int(run_t/self.dt)):
@@ -17,6 +19,8 @@ def collision(self,theta0,run_t):
         ball.pos.x=ball.pos.x+ball.velocity.x*self.dt
         ball.pos.y=ball.pos.y+ball.velocity.y*self.dt
         ball.trail.append(pos=ball.pos)
+        if fabs(ball.pos.y-0)<=0.001:
+            x.append(ball.pos.x);v_x.append(ball.velocity.x)
         if fabs(0.0025*ball.pos.x**2+0.01*ball.pos.y**2-1)<0.001:
             norm=sqrt((4*ball.pos.y)**2+ball.pos.x**2)
             vect_n=vector(ball.pos.x/norm,4*ball.pos.y/norm)
@@ -80,7 +84,20 @@ def collision(self,theta0,run_t):
                     elif ball.pos.x<0 and ball.pos.y<0:
                         vect_f=-ball.velocity+vector(2*4*ball.pos.y/norm*addon,-2*ball.pos.x/norm*addon)
                         ball.velocity=vect_f
-                        
-ball=Billiard()
-__init__(ball,0.001)
-collision(ball,1,5000)
+    return (x,v_x)                        
+def phase_space(run_t):
+    ball=Billiard()
+    dt=0.001
+    xx=[];v_xx=[]
+    __init__(ball,0.001)
+    lis=collision(ball,1,run_t)
+    xx=lis[0];v_xx=lis[1]
+    title('Elliptical phase_space')
+    xlabel('x');ylabel('v_x')
+    scatter(xx,v_xx,color='black',s=1,label='initial pos:focal point')
+    legend(loc='upper right',fontsize=11)
+    show()        
+phase_space(1000)
+        
+        
+    
